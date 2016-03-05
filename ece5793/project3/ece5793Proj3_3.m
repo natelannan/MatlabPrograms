da918820b341d27ffc420ff35b912d44e717fc68
natelannan <natelannan@gmail.com>
2016-03-04 23:33:00 -0600

    added padding and lines

--- ece5793/project3/ece5793Proj3_3.m
+++ ece5793/project3/ece5793Proj3_3.m
@@ -19,14 +19,18 @@ clc
 
 %-------Read in images and initialize--------------------------------------
 lena=imread('leangray.bmp');
-[Nlena,Mlena,Z]=size(lena);
-L=256;
+lenaInfo=imfinfo('leangray.bmp');
+
 llena=im2double(lena);
 
 clown=imread('clowngray.bmp');
-[Nclown,Mclown,Z]=size(clown);
+clownInfo=imfinfo('clowngray.bmp');
+
 L=256;
+padSize=294;
 cclown=im2double(clown);
+cclown=padarray(cclown,[padSize padSize]);
+
 
 CLOWN = log(abs(fftshift(fft2(cclown)))+1);
 FFTCLOWN=fftshift(fft2(cclown));
@@ -38,7 +42,7 @@ figure(1)
 subplot(2,2,1)
 imshow(lena)
 subplot(2,2,2)
-imshow(clown)
+imshow(cclown)
 subplot(2,2,3)
 imshow(LENA,[])
 subplot(2,2,4)
@@ -64,14 +68,14 @@ imshow(CLOWN,[])
 % bar=abs(ifft2(ifftshift(filtered)));
 % figure(5)
 % imshow(bar)
-centerL=[Nlena/2 Mlena/2];
-centerC=[Nclown/2 Mclown/2];
-notch=ones(Mlena);
-notch2=ones(Mclown);
-u=1:256; v=1:256;
+centerL=[lenaInfo.Height/2+1 lenaInfo.Width/2+1];
+centerC=[(clownInfo.Height+2*padSize)/2+1 (clownInfo.Width+2*padSize)/2+1];
+notch=ones(lenaInfo.Height);
+notch2=ones(clownInfo.Height+2*padSize);
+u=1:lenaInfo.Height; v=1:lenaInfo.Width;
 [XI, YI] = ndgrid(u,v);
 % D=sqrt((XI-128).^2+(YI-128).^2);
-pts=[127 111 95 79 63 47 31 15 -17 -33 -49 -65 -81 -97 -113];
+pts=[128 112 96 80 64 48 32 16 -16 -32 -48 -64 -80 -96 -112 -128];
 for i=1:length(pts)
     D=sqrt((XI-centerL(2)+pts(i)).^2+(YI-centerL(1)+pts(i)).^2);
     notch(D<1)=0;
@@ -92,20 +96,27 @@ imshow(reversed)
 figure(9)
 imshow(lena)
 
-u=1:294; v=1:294;
+u=1:clownInfo.Height+2*padSize; v=1:clownInfo.Width+2*padSize;
 [XI, YI] = ndgrid(u,v);
-D1=sqrt((XI-centerC(2)+(centerC(2)-123)).^2+(YI-centerC(1)+(centerC(1)-191)).^2);
-D2=sqrt((XI-centerC(2)+(centerC(2)-136)).^2+(YI-centerC(1)+(centerC(1)-127)).^2);
-D3=sqrt((XI-centerC(2)+(centerC(2)-173)).^2+(YI-centerC(1)+(centerC(1)-105)).^2);
-D4=sqrt((XI-centerC(2)+(centerC(2)-160)).^2+(YI-centerC(1)+(centerC(1)-169)).^2);
-notch2(D1<=4)=0; notch2(D3<=4)=0;
-notch2(D2<=3)=0; notch2(D4<=3)=0;
+D1=sqrt((XI-centerC(2)+(centerC(2)-405)).^2+(YI-centerC(1)+(centerC(1)-378)).^2);
+D2=sqrt((XI-centerC(2)+(centerC(2)-368)).^2+(YI-centerC(1)+(centerC(1)-570)).^2);
+D3=sqrt((XI-centerC(2)+(centerC(2)-516)).^2+(YI-centerC(1)+(centerC(1)-314)).^2);
+D4=sqrt((XI-centerC(2)+(centerC(2)-479)).^2+(YI-centerC(1)+(centerC(1)-506)).^2);
+D5=abs(XI-405); D6=abs(YI-378);D7=abs(XI-368); D8=abs(YI-570);
+D9=abs(XI-516); D10=abs(YI-314);D11=abs(XI-479); D12=abs(YI-506);
+notch2(D1<=24)=0; notch2(D4<=24)=0;
+notch2(D2<=36)=0; notch2(D3<=36)=0;
+notch2((D5<1|D6<1)&(abs(YI-centerC(1))>4)&(abs(XI-centerC(2))>4))=0;
+notch2((D7<1|D8<1)&(abs(YI-centerC(1))>4)&(abs(XI-centerC(2))>4))=0;
+notch2((D9<1|D10<1)&(abs(YI-centerC(1))>4)&(abs(XI-centerC(2))>4))=0;
+notch2((D11<1|D12<1)&(abs(YI-centerC(1))>4)&(abs(XI-centerC(2))>4))=0;
 figure(10)
 imshow(notch2)
 filtered=FFTCLOWN.*notch2;
 figure(11)
 imshow(log(abs(filtered)+1),[])
 reversed=abs(ifft2(ifftshift(filtered)));
+reversed=imcrop(reversed,[padSize+1 padSize+1 padSize-1 padSize-1]);
 figure(12)
 imshow(reversed)
 figure(13)
